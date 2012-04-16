a8ed7495c0b868dac027bfa5584e3bb1e6c4c14b
Travis McCauley <travis.mccauley@gmail.com>
2012-04-16 16:10:32 -0400

    added a query with order by

--- scripts/fix-empty-node-descs.php
+++ scripts/fix-empty-node-descs.php
@@ -2,6 +2,9 @@
 $sql = "SELECT entity_id FROM field_data_field_description
    WHERE field_description_value = ''";
 $etids = db_query($sql)->fetchCol();
+var_dump($etids);
 entity_delete_multiple('field_collection_item',$etids);
 
 
+SELECT length(field_description_value) AS len, field_description_value FROM field_data_field_description
+   order by len ASC 

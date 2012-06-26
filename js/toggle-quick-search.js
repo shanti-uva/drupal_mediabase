0ab051bbcd173de6e6e471c38d4bc1916db528b7
Travis McCauley <travis.mccauley@gmail.com>
2012-06-26 17:37:09 -0400

    ui improvements for search toggle box - take 2

--- js/toggle-quick-search.js
+++ js/toggle-quick-search.js
@@ -11,7 +11,7 @@ Drupal.behaviors.kmap_taxonomy={attach:function(context){
       searchForm.find('form').append(toggleBlock);
       
       // close button
-      toggleBlock.append( jQuery('<a>[x]</a>').attr({
+   toggleBlock.append( jQuery('<a>[x]</a>').attr({
             href: '#',
             class: 'close-toggle-box',
       }).click(function(event) {
@@ -21,60 +21,59 @@ Drupal.behaviors.kmap_taxonomy={attach:function(context){
       );
       
       // site search button
+      var radioId = 'site-radio-' + searchForm.attr('id')
       toggleBlock.append( jQuery('<input/>').attr({
             type: 'radio',
             name: 'search-type',
             value: 'site',
-            checked: true
+            checked: true,
+            id: radioId,
       }).change( function (event) {
-         //console.log('site change')
          toggleSearchForm(this);
       }
       ));
-      toggleBlock.append( jQuery('<span/>').text(Drupal.t('Site Search')));
+      toggleBlock.append( jQuery('<label/>').text(Drupal.t('Site Search')).attr('for', radioId ));
       toggleBlock.append( jQuery('<br/>'));
       
       // transcript search button
+      radioId = 'transcript-radio-' + searchForm.attr('id')
       toggleBlock.append( jQuery('<input/>').attr({
             type: 'radio',
             name: 'search-type',
             value: 'transcript',
+            id: radioId,
       }).change( function (event) {
-         //console.log('transcript change')
          toggleSearchForm(this);
       }
       ));
       
-      toggleBlock.append( jQuery('<span/>').text(Drupal.t('Transcript Search')));
+      toggleBlock.append( jQuery('<label/>').text(Drupal.t('Transcript Search')).attr('for', radioId ));
       
-      searchForm.hover( function(event) {
+      searchForm.find('input[type=text]').hover( function(event) {
             toggleBlock.addClass('visible');
       });
    }
    
    function toggleSearchForm(input) {
-      console.log('toggling')
-      
       var transcriptSearch = jQuery('#block-transcripts-transcript-search');
       var siteSearch = jQuery('#block-search-form');
-      
-      console.log('input.value', input.value)
       var termValue = jQuery(input).parents('form').find('input[type=text]').val()
-      console.log('termValue', termValue)
-      
       if (input.value == 'transcript' ) {
          transcriptSearch.addClass('active');
          transcriptSearch.find('input[value=transcript]').attr('checked',true)
-         siteSearch.removeClass('active');
-         siteSearch.find('input[value=site]').attr('checked',true)
          transcriptSearch.find('input[type=text]').val( termValue )
          transcriptSearch.find('.search-toggle-box').addClass('visible');
+         siteSearch.removeClass('active');
+         siteSearch.find('input[value=site]').attr('checked',true)
          siteSearch.find('.search-toggle-box').removeClass('visible');
       } else {
          siteSearch.addClass('active');
-         transcriptSearch.removeClass('active');
+         siteSearch.find('input[value=site]').attr('checked',true)
          siteSearch.find('input[type=text]').val( termValue )
+         siteSearch.find('.search-toggle-box').addClass('visible');
+         transcriptSearch.removeClass('active');
+         transcriptSearch.find('input[value=transcript]').attr('checked',true)
+         transcriptSearch.find('.search-toggle-box').removeClass('visible');
       }
    }
-   
 }};

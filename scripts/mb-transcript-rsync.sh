75c3765aa4581d875ba7592e8604bea116429fe2
Travis McCauley <travis.mccauley@gmail.com>
2013-01-10 16:18:01 -0800

    added a shell script to transfer transcripts to workaround indexing bug in cron

---
+++ scripts/mb-transcript-rsync.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+drush_alias="@mediabase.drupal.shanti.virginia.edu"
+drush_status=`drush $drush_alias status drupal_root --pipe`
+drupal_root="${drush_status//[[:space:]]/}" # use parameter expansion and trim whitespace
+rsync aegir@drupal.shanti.virginia.edu:$drupal_root/sites/mediabase.drupal.shanti.virginia.edu/files/transcripts/* $drupal_root/sites/mediabase.drupal.shanti.virginia.edu/files/transcripts/
+

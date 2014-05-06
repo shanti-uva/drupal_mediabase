677511b630c04cb369e8e02a2deabb57c592233f
ThanGrove <than@virginia.edu>
2014-05-06 15:45:25 -0400

    Update to how thumbnails are created and update to breadcrumb code

---
+++ templates/mediabase_thumbnail.tpl.php
@@ -0,0 +1,25 @@
+<div class="shanti-thumbnail">
+    <div class="shanti-thumbnail-field shanti-field-<?php print $entity['bundle']; ?>">        
+      <div class="shanti-field-content"><a href="<?php print $entity['link']; ?>" 
+        class="shanti-thumbnail-link">
+        <div class="kaltura">
+            <div class="kaltura-thumb"><img title="<?php print $entity['title']; ?>" alt="<?php print $entity['title']; ?>" 
+          src="<?php print $entity['thumb_url']; ?>" 
+          typeof="foaf:Image" class="k-no-rotate"></div>
+        </div></a></div> 
+    </div>  
+    <div class="shanti-thumbnail-field shanti-field-title">        
+       <span class="field-content"><a href="<?php print $entity['link']; ?>" 
+           class="shanti-thumbnail-link"><?php print $entity['title']; ?></a></span>  
+    </div>  
+    <div class="shanti-field shanti-field-created">    
+        <span class="shanti-label shanti-label-created">Posted</span>    
+        <span class="shanti-field-content"><?php print date('M jS, Y', $entity['fields']['created']); ?></span>  
+    </div>  
+    <div class="shanti-field shanti-field-group-audience">    
+        <span class="shanti-label shanti-label-group-audience">in </span>    
+        <div class="shanti-field-content"><a href="<?php print $coll->url; ?>" 
+          class="shanti-thumbnail-link"><a href="<? print $coll->url; ?>"><?php print $coll->title; ?></a>
+        </div>  
+    </div>  
+</div>

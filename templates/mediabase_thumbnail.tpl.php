a6912291716b74b65fae3edc3652bb680fa784b2
ThanGrove <than@virginia.edu>
2014-07-28 12:39:48 -0400

    Updates from Mark to Mediabase templates

--- templates/mediabase_thumbnail.tpl.php
+++ templates/mediabase_thumbnail.tpl.php
@@ -1,6 +1,9 @@
 <div class="shanti-thumbnail <?php print $result['bundle']; ?> col-lg-2 col-md-3 col-sm-4 col-xs-12"> 
     <div class="shanti-thumbnail-image shanti-field-<?php print $result['bundle']; ?>"> 
       <a href="<?php print $result['link']; ?>" class="shanti-thumbnail-link">
+         <span class="overlay">
+            <span class="icon"></span>
+         </span>
          <img title="<?php print $result['title']; ?>" 
              alt="<?php print $result['title']; ?>" 
              src="<?php if(isset($result['thumb_url'])) { print $result['thumb_url']; } ?>" 
@@ -8,7 +11,8 @@
          <i class="shanticon-<?php if($result['bundle'] == 'video') { print 'video'; } else { print 'audio'; } ?> thumbtype"></i> <!-- Need to switch icon based on bundle -->
       </a>
     </div>
-    <div class="shanti-thumbnail-info">
+    <div class="shanti-thumbnail-info">      
+     <div class="body-wrap">
       <div class="shanti-thumbnail-field shanti-field-created">       
           <span class="shanti-field-content"><?php print date('j F Y', $result['fields']['created']); ?></span>  
       </div>  
@@ -22,14 +26,7 @@
         </div>
       <?php endif; ?>
       
-      <?php if(isset($result['place_link']) && count($result['place_link']) > 0): ?>
-        <div class="shanti-thumbnail-field shanti-field-place">        
-         <span class="field-content"><i class="shanticon shanticon-places"></i> 
-           <?php print render($result['place_link']); ?>
-         </span>
-        </div>
-      <?php endif; ?>
-      
+            
       <?php if($coll && 2 == 1): ?>
         <div class="shanti-field shanti-field-group-audience">    
             <span class="shanti-label shanti-label-group-audience">in </span>    
@@ -38,5 +35,16 @@
             </div>  
         </div>  
       <?php endif; ?>
-    </div>  
-</div>
+    </div> <!-- end body-wrap -->
+    
+    <div class="footer-wrap">  
+      <?php if(isset($result['place_link']) && count($result['place_link']) > 0): ?>
+        <div class="shanti-thumbnail-field shanti-field-place">        
+         <span class="field-content"><i class="shanticon shanticon-places"></i> 
+           <?php print render($result['place_link']); ?>
+         </span>
+        </div>
+      <?php endif; ?>
+    </div> <!-- end footer -->  
+   </div> <!-- end shanti-thumbnail-info -->
+</div> <!-- end shanti-thumbnail -->

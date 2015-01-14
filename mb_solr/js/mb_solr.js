85f48c1a2b0b799662f1b55f88351109e32ca8cd
ThanGrove <than@virginia.edu>
2015-01-14 14:06:16 -0500

    updates to facet tree in blocks in sidebar to display in tabs

--- mb_solr/js/mb_solr.js
+++ mb_solr/js/mb_solr.js
@@ -23,23 +23,26 @@
 	// Called by ajax_command_invoke in mb_solr.module from mb_solr_facets_ajax() function
 	$.fn.updateFacetTree = function(data) {
 		var fsel = Drupal.settings.mediabase.facets;
-		console.log(fsel + " - facet selected");
+		var fid = fsel.split(":").pop();
 		for(var fname in Drupal.settings.mediabase.facetcounts) {
-			var tree = jQuery('.facet-' + fname).fancytree('getTree');
-			tree.clearFilter();
-			var fcounts = Drupal.settings.mediabase.facetcounts[fname];
-			var root = tree.getRootNode();
-			root.visit(function(node) {
-				if(fcounts.hasOwnProperty(node.data.fid)) {
-					node.data.count = fcounts[node.data.fid];
-					if(fsel.indexOf(':' + node.data.fid) > -1) { node.setSelected(); }
-					node.setExpanded();
-				}
-		  });
-		  var ct = tree.filterNodes(function(node) { 
-				var showit = fcounts.hasOwnProperty(node.data.fid);
-				return showit;
-			});
+			var flabel = fname.split('_').pop();
+			var tree = jQuery('.facet-' + flabel + ' .content').fancytree('getTree');
+			if(typeof(tree) != 'undefined' && typeof(tree.widget) != "undefined") {
+				tree.clearFilter();
+				var fcounts = Drupal.settings.mediabase.facetcounts[fname];
+				var root = tree.getRootNode();
+				root.visit(function(node) {
+					if(fcounts.hasOwnProperty(node.data.fid)) {
+						node.data.count = fcounts[node.data.fid];
+						if(fsel.indexOf(':' + node.data.fid) > -1) { node.setSelected(); }
+						node.setExpanded();
+					}
+			  });
+			  var ct = tree.filterNodes(function(node) { 
+					var showit = fcounts.hasOwnProperty(node.data.fid);
+					return showit;
+				});
+			}
 		}
 		
 	};

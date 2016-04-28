2217c2980d5d0cd578afc649423d112570652d8f
ThanGrove <than@virginia.edu>
2016-04-28 17:44:24 -0400

    Update to mediabases.js for better organization and sorting collection names in form

--- js/mediabase.js
+++ js/mediabase.js
@@ -1,20 +1,9 @@
 (function ($) {
-	Drupal.behaviors.mediabase={
+	Drupal.behaviors.mediabaseImages={
 		attach: function(context){
 		
 		   fixAudioImages();
 		   replaceBrokenImages(); 
-		   fixVBOCheckBoxes();
-		   if ($('body').hasClass('page-mycontent-workflow')) { 
-		       cleanupWorkflowSelects(context);  
-		   }
-		   
-			// Code to add transcript and description radiobuttons to search form
-		   var transcriptSearch = jQuery('#block-transcripts-transcript-search');
-		   var siteSearch = jQuery('#block-search-form');
-		   siteSearch.addClass('active');
-		   addToggleToSearchForm( transcriptSearch);
-		   addToggleToSearchForm( siteSearch);
 		   
 			function fixAudioImages() {
 				$('.shanti-thumbnail.audio img').each(function() { 
@@ -26,90 +15,6 @@
 					} 
 				});
 			}
-		   
-		   function fixVBOCheckBoxes() {
-		   		// vbo click anywhere in the row to enable messes up the ICheck function so disabling on view-my-content forms
-		   		$('.view-my-content .views-table tbody tr').unbind('click');
-			   }
-		   
-		   function addToggleToSearchForm( searchForm ) {
-		   	return; // This should be done in Drupal with the Form API and possibly a submit function. Disabling for now.
-		      var toggleBlock = jQuery('<p class="search-toggle-box"/>');
-		      searchForm.find('form').append(toggleBlock);
-		      
-		      // site search button
-		      var radioId = 'site-radio-' + searchForm.attr('id');
-		      toggleBlock.append( jQuery('<input/>').attr({
-		            type: 'radio',
-		            name: 'search-type',
-		            value: 'site',
-		            checked: true,
-		            id: radioId,
-		      }).change( function (event) {
-		         toggleSearchForm(this);
-		      }
-		      ));
-		      toggleBlock.append( jQuery('<label/>').text(Drupal.t('Site Search')).attr('for', radioId ));
-		      toggleBlock.append( jQuery('<br/>'));
-		      
-		      // transcript search button
-		      radioId = 'transcript-radio-' + searchForm.attr('id');
-		      toggleBlock.append( jQuery('<input/>').attr({
-		            type: 'radio',
-		            name: 'search-type',
-		            value: 'transcript',
-		            id: radioId,
-		      }).change( function (event) {
-		         toggleSearchForm(this);
-		      }
-		      ));
-		      toggleBlock.append( jQuery('<label/>').text(Drupal.t('Transcript Search')).attr('for', radioId ));
-		      
-		      // add the close link
-		      toggleBlock.prepend( jQuery('<a title="close"><img/></a>').attr({
-		            href: '#',
-		            class: 'close-toggle-box',
-		      }).css({
-		            float: 'right',
-		            width: '20px',
-		      }).click(function(event) {
-		         toggleBlock.removeClass('visible');
-		         event.preventDefault();
-		      }));
-		      
-		      // add the close button
-		      var closeButtonPath = Drupal.settings.basePath +  Drupal.settings.mediabase.path + '/images/close.png';
-		      jQuery('.close-toggle-box img').attr( {
-		            src: closeButtonPath,
-		      }).css('width', '20px');
-		      
-		      searchForm.find('input[type=text]').hover( function(event) {
-		            toggleBlock.addClass('visible');
-		      });
-		   }
-		   
-		   function toggleSearchForm(input) {
-		      var transcriptSearch = jQuery('#block-transcripts-transcript-search');
-		      var siteSearch = jQuery('#block-search-form');
-		      var termValue = jQuery(input).parents('form').find('input[type=text]').val();
-		      if (input.value == 'transcript' ) {
-		         transcriptSearch.addClass('active');
-		         transcriptSearch.find('input[value=transcript]').attr('checked',true);
-		         transcriptSearch.find('input[type=text]').val( termValue );
-		         transcriptSearch.find('.search-toggle-box').addClass('visible');
-		         siteSearch.removeClass('active');
-		         siteSearch.find('input[value=site]').attr('checked',true);
-		         siteSearch.find('.search-toggle-box').removeClass('visible');
-		      } else {
-		         siteSearch.addClass('active');
-		         siteSearch.find('input[value=site]').attr('checked',true);
-		         siteSearch.find('input[type=text]').val( termValue );
-		         siteSearch.find('.search-toggle-box').addClass('visible');
-		         transcriptSearch.removeClass('active');
-		         transcriptSearch.find('input[value=transcript]').attr('checked',true);
-		         transcriptSearch.find('.search-toggle-box').removeClass('visible');
-		      }
-		   }
 		   
 		   // On image error in thumbnail div, use the default blank thumbnail image
 		   function replaceBrokenImages() {
@@ -123,8 +28,116 @@
 		       }
 		     });
 		   }
-		   
-		   // Arrange the 19 selects on the workflow page into organized divs for formatting
+		}
+	};
+	
+	Drupal.behaviors.mediabaseSearch={
+        attach: function(context){
+           
+            // Code to add transcript and description radiobuttons to search form
+           var transcriptSearch = jQuery('#block-transcripts-transcript-search');
+           var siteSearch = jQuery('#block-search-form');
+           siteSearch.addClass('active');
+           addToggleToSearchForm( transcriptSearch);
+           addToggleToSearchForm( siteSearch);
+           
+           function addToggleToSearchForm( searchForm ) {
+            return; // This should be done in Drupal with the Form API and possibly a submit function. Disabling for now.
+              var toggleBlock = jQuery('<p class="search-toggle-box"/>');
+              searchForm.find('form').append(toggleBlock);
+              
+              // site search button
+              var radioId = 'site-radio-' + searchForm.attr('id');
+              toggleBlock.append( jQuery('<input/>').attr({
+                    type: 'radio',
+                    name: 'search-type',
+                    value: 'site',
+                    checked: true,
+                    id: radioId,
+              }).change( function (event) {
+                 toggleSearchForm(this);
+              }
+              ));
+              toggleBlock.append( jQuery('<label/>').text(Drupal.t('Site Search')).attr('for', radioId ));
+              toggleBlock.append( jQuery('<br/>'));
+              
+              // transcript search button
+              radioId = 'transcript-radio-' + searchForm.attr('id');
+              toggleBlock.append( jQuery('<input/>').attr({
+                    type: 'radio',
+                    name: 'search-type',
+                    value: 'transcript',
+                    id: radioId,
+              }).change( function (event) {
+                 toggleSearchForm(this);
+              }
+              ));
+              toggleBlock.append( jQuery('<label/>').text(Drupal.t('Transcript Search')).attr('for', radioId ));
+              
+              // add the close link
+              toggleBlock.prepend( jQuery('<a title="close"><img/></a>').attr({
+                    href: '#',
+                    class: 'close-toggle-box',
+              }).css({
+                    float: 'right',
+                    width: '20px',
+              }).click(function(event) {
+                 toggleBlock.removeClass('visible');
+                 event.preventDefault();
+              }));
+              
+              // add the close button
+              var closeButtonPath = Drupal.settings.basePath +  Drupal.settings.mediabase.path + '/images/close.png';
+              jQuery('.close-toggle-box img').attr( {
+                    src: closeButtonPath,
+              }).css('width', '20px');
+              
+              searchForm.find('input[type=text]').hover( function(event) {
+                    toggleBlock.addClass('visible');
+              });
+           }
+           
+           function toggleSearchForm(input) {
+              var transcriptSearch = jQuery('#block-transcripts-transcript-search');
+              var siteSearch = jQuery('#block-search-form');
+              var termValue = jQuery(input).parents('form').find('input[type=text]').val();
+              if (input.value == 'transcript' ) {
+                 transcriptSearch.addClass('active');
+                 transcriptSearch.find('input[value=transcript]').attr('checked',true);
+                 transcriptSearch.find('input[type=text]').val( termValue );
+                 transcriptSearch.find('.search-toggle-box').addClass('visible');
+                 siteSearch.removeClass('active');
+                 siteSearch.find('input[value=site]').attr('checked',true);
+                 siteSearch.find('.search-toggle-box').removeClass('visible');
+              } else {
+                 siteSearch.addClass('active');
+                 siteSearch.find('input[value=site]').attr('checked',true);
+                 siteSearch.find('input[type=text]').val( termValue );
+                 siteSearch.find('.search-toggle-box').addClass('visible');
+                 transcriptSearch.removeClass('active');
+                 transcriptSearch.find('input[value=transcript]').attr('checked',true);
+                 transcriptSearch.find('.search-toggle-box').removeClass('visible');
+              }
+           }
+           
+           
+        }
+    };
+    
+    Drupal.behaviors.mediabaseViews={
+        attach: function(context){
+             
+           fixVBOCheckBoxes();
+           if ($('body').hasClass('page-mycontent-workflow')) { 
+               cleanupWorkflowSelects(context);  
+           }
+           
+           function fixVBOCheckBoxes() {
+                // vbo click anywhere in the row to enable messes up the ICheck function so disabling on view-my-content forms
+                $('.view-my-content .views-table tbody tr').unbind('click');
+               }
+           
+           // Arrange the 19 selects on the workflow page into organized divs for formatting
             function cleanupWorkflowSelects() {
                 // Remove all the select widgets into groups
                 var grp3 = $('#views-exposed-form-my-workflow-workflow-all .views-exposed-widget').detach();
@@ -185,9 +198,14 @@
                 accordionGrp.append(row3);
                 accordionGrp.after(genfilters);
             }
-		   
-		}
-	};
-	
+        }
+    };
+    
+     Drupal.behaviors.mediabaseForms={
+        attach: function(context){
+            // Disable submit forms once something is submitted
+           $('#video-node-form, #audio-node-form').submit(function()  { $('#edit-submit').prop('disabled',true);});
+        }
+     };
 
 } (jQuery));
